0c6074eaf72bc8823a2559b378628cc39f2a97ce
vdBerg93 <anonymous@a.com>
2021-07-14 15:11:02 +0200

    Did assignment 1

--- assignment1.py
+++ assignment1.py
@@ -10,19 +10,28 @@ class ModelPredictiveControl:
     def __init__(self):
         self.horizon = 20
         self.dt = 0.2
-
+        self.vmax = 10/3.6
+        self.w_dist = 1
+        self.w_speed = 10000
         # Reference or set point the controller will achieve.
         self.reference = [50, 0, 0]
 
     def plant_model(self, prev_state, dt, pedal, steering):
         x_t = prev_state[0]
         v_t = prev_state[3] # m/s
+        x_t_1 = x_t + v_t*dt
+        v_t_1 = 0.95*v_t + pedal
         return [x_t_1, 0, 0, v_t_1]
 
     def cost_function(self,u, *args):
         state = args[0]
         ref = args[1]
-        cost = 0.0
+        cost = 0
+        for u_i in u:
+            state = self.plant_model(state, self.dt, u_i, None)
+            d_goal = ref[0]-state[0]
+            err_speed = state[3]-self.vmax
+            cost += pow(d_goal,2) + (err_speed>0)*self.w_speed*err_speed
         return cost
 
 sim_run(options, ModelPredictiveControl)
